a7fc54c5b47c90f309be0ac1e03509adc6bbc2d2
minhmc191109-droid <minhmc191109@gmail.com>
2026-09-23 12:36:34 +0700

    Add files via upload

--- js/locations.js
+++ js/locations.js
@@ -74,7 +74,7 @@ const locationsData = {
             name: "Khu du lịch Tràng An, Ninh Bình",
             lat: 20.2526,
             lng: 105.9142,
-            imageUrl: "https://upload.wikimedia.org/wikipedia/commons/d/df/Trang_An_Landscape_Complex_Panorama.jpg"
+            imageUrl: "https://www.bachhoaxanh.com/kinh-nghiem-hay/kinh-nghiem-du-lich-trang-an-ninh-binh-day-thu-vi-tiet-kiem-1365422"
         }
     ],
     
